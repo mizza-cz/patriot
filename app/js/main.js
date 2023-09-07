6342b59785cbac89803f9ad0f921df48fc33b22b
mizza-cz <mizza.cz666@gmail.com>
2023-09-07 15:55:43 +0200

    fix gallery

--- app/js/main.js
+++ app/js/main.js
@@ -6365,40 +6365,32 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(nextSlide, 5000);
 });
 
-$(document).ready(function () {
-  var a = !1;
-  $('a[data-gallery]').click(function () {
-    if (!a) {
-      a = !0;
-      var r = $(this).data('gallery'),
-        s = $(this).data('start'),
-        t = '/inc/gallery-ajax.php?mode=' + r;
-      return (
-        (1 != r && 2 != r && 3 != r) || (t += '&id=' + $(this).data('gallery-id')),
-        $.getJSON(t, {
-          format: 'json',
-        })
-          .done(function (a) {
-            $(this).lightGallery({
-              hash: !1,
-              share: !1,
-              dynamic: !0,
-              dynamicEl: a,
-              index: s,
-              download: !1,
-              backdropDuration: 500,
-            });
-          })
-          .fail(function (a, r, t) {
-            alert('Nastala chyba při načítání galerie. Prosím zkuste to znovu.'),
-              console.error('getJSON failed, status: ' + r + ', error: ' + t),
-              console.error(a);
-          })
-          .always(function () {
-            a = !1;
-          }),
-        !1
-      );
-    }
+$('#animated-thumbnail').lightGallery({
+  thumbnail: true,
+  getCaptionFromTitleOrAlt: true,
+  selector: 'a[style]',
+});
+// Image Transition
+var scroll = 'yes',
+  Fscroll = scroll.replace(/(\r\n|\n|\r)/gm, ' ');
+'yes' === Fscroll &&
+  ($(document).ready(function () {
+    $('body').addClass('imgani');
+  }),
+  $(window).bind('load resize scroll', function () {
+    var o = $(this).height();
+    $('img').each(function () {
+      var s = 0.1 * $(this).height() - o + $(this).offset().top;
+      $(document).scrollTop() > s && $(this).addClass('anime');
+    });
+  }));
+$(function () {
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('body').on('click', '.nav-tabs a', function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    $(this).tab('show');
   });
 });
